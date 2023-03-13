fe06b519f945f8204209c28c643f5bde5e2dfedc
Viazovskyi Stas <viazovskyi.stas@gmail.com>
2023-03-13 21:43:47 +0200

    ignore + graphql + generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { } from "@/graphql/generated"
 
 const inter = Inter({ subsets: ['latin'] })
 
